804b144b5a8ea96658af5e2b220eb2aabdd9a81e
motalibpathan <motalibpathan11@gmail.com>
2023-08-09 00:06:37 +0600

    upload issue solved

--- helpers/imageUpload.js
+++ helpers/imageUpload.js
@@ -1,11 +1,19 @@
-// /helpers/imageUpload.js
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs"); // Import the fs module
+
+// Define the path to the uploads folder
+const uploadsFolderPath = path.join(__dirname, "../uploads");
+
+// Create the uploads folder if it doesn't exist
+if (!fs.existsSync(uploadsFolderPath)) {
+  fs.mkdirSync(uploadsFolderPath);
+}
 
 // Define storage for the uploaded images
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "../uploads")); // Adjust the destination path as needed
+    cb(null, uploadsFolderPath); // Use the uploadsFolderPath
   },
   filename: function (req, file, cb) {
     const uniqueSuffix =
